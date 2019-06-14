6449570fa949495d0fc396df9f5f9c9435e51d4b
yumezouWebTeam <t.osada@kusamura.org>
2019-06-14 10:35:09 +0900

    __toStringの処理をビルドインに準拠するように修正

--- Core/Exception/ClassUndefinedException.class.php
+++ Core/Exception/ClassUndefinedException.class.php
@@ -71,7 +71,7 @@ class ClassUndefinedException extends Exception {
      */
     public function __toString()
     {
-        return sprintf("[Exception '%s' with message '%s' in '%s]", __CLASS__, $this->getErrorMessage(), join(":", [$this->getFile(), $this->getLine()]));
+        return sprintf("Exception '%s' with message '%s' in '%s'", __CLASS__, $this->getErrorMessage(), join(":", [$this->getFile(), $this->getLine()]));
     }
 
 }
