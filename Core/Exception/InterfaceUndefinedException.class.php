3d792f15eb4065336e592c4ca37f8780596e0b11
yumezouWebTeam <t.osada@kusamura.org>
2019-06-19 10:43:50 +0900

    __toStringの処理をビルドインに準拠するように修正

--- Core/Exception/InterfaceUndefinedException.class.php
+++ Core/Exception/InterfaceUndefinedException.class.php
@@ -70,7 +70,7 @@ class InterfaceUndefinedException extends Exception {
      */
     public function __toString()
     {
-        return join("::", [__CLASS__, $this->getErrorMessage()]);
+        return sprintf("Exception '%s' with message '%s' in '%s'", __CLASS__, $this->getErrorMessage(), join(":", [$this->getFile(), $this->getLine()]));
     }
 
 }
